e0a44c92728ea24d5531d275f0b6903f4dd9b70a
P. PRANEETH REDDY <praneethr1000@gmail.com>
2020-06-17 09:03:06 +0530

    Delete StringsOfLength"K".py

--- Recursion/StringsOfLength"K".py
+++
@@ -1,9 +0,0 @@
-def rangeToList(k):
-    return [str(l) for l in range(k)]
-def baseKStrings(n,k):
-    if n == 0:
-        return []
-    if n == 1:
-        return rangeToList(k)
-    return [digit+bitstring for digit in baseKStrings(1,k) for bitstring in baseKStrings(n-1,k)]
-print(baseKStrings(3,2))
